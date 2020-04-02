425223154180e4df808bff31bcd1fe91d44973ea
root <Chris@hedia.co>
2020-04-02 19:37:40 +0100

    Setting up Typescript, tslint & prettier

--- src/components/LoadingBar.js
+++ src/components/LoadingBar.js
@@ -1,4 +1,5 @@
-import styled, { keyframes } from 'react-emotion';
+import styled from "@emotion/styled";
+import { keyframes } from "@emotion/core";
 
 const frames = keyframes`
   0% {
@@ -8,28 +9,28 @@ const frames = keyframes`
     transform: translateX(-400px)
   }
 `;
-const animationColor1 = 'rgba(74, 242, 161, 1.00)';
-const animationColor2 = 'rgba(140, 169, 197, 1.00)';
+const animationColor1 = "rgba(74, 242, 161, 1.00)";
+const animationColor2 = "rgba(140, 169, 197, 1.00)";
 
-export default styled('div')`
-  display: block;
-  animation-name: ${frames};
-  animation-duration: 2s;
-  animation-timing-function: linear;
-  animation-iteration-count: infinite;
-  background-color: ${animationColor1};
-  background-image: linear-gradient(
-    to right,
-    ${animationColor2} 0,
-    ${animationColor2} 50%,
-    ${animationColor1} 50%,
-    ${animationColor1} 100%
-  );
-  background-repeat: repeat-x;
-  background-size: 25pc 25pc;
-  width: 200%;
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 2;
+export default styled("div")`
+	display: block;
+	animation-name: ${frames};
+	animation-duration: 2s;
+	animation-timing-function: linear;
+	animation-iteration-count: infinite;
+	background-color: ${animationColor1};
+	background-image: linear-gradient(
+		to right,
+		${animationColor2} 0,
+		${animationColor2} 50%,
+		${animationColor1} 50%,
+		${animationColor1} 100%
+	);
+	background-repeat: repeat-x;
+	background-size: 25pc 25pc;
+	width: 200%;
+	position: fixed;
+	top: 0;
+	left: 0;
+	height: 2;
 `;
---
+++ src/components/MessageBytes.tsx
@@ -0,0 +1,133 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+
+export default class MessageBytes extends Component {
+	public static propTypes = {
+		seekTime: PropTypes.number.isRequired,
+		message: PropTypes.object.isRequired,
+		seekIndex: PropTypes.number,
+		live: PropTypes.bool.isRequired,
+	};
+
+	private canvas: HTMLCanvasElement;
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			isVisible: true,
+			lastMessageIndex: 0,
+			lastSeekTime: 0,
+		};
+
+		this.onVisibilityChange = this.onVisibilityChange.bind(this);
+		this.onCanvasRefAvailable = this.onCanvasRefAvailable.bind(this);
+	}
+
+	public shouldComponentUpdate(nextProps, nextState) {
+		if (nextProps.live) {
+			const nextLastEntry = nextProps.message.entries[nextProps.message.entries.length - 1];
+			const curLastEntry = this.props.message.entries[this.props.message.entries.length - 1];
+
+			return nextLastEntry.hexData !== curLastEntry.hexData;
+		}
+		return nextProps.seekTime !== this.props.seekTime;
+	}
+
+	public componentWillReceiveProps(nextProps) {
+		if (
+			this.props.seekIndex !== nextProps.seekIndex ||
+			frameForTime(this.props.seekTime) !== frameForTime(nextProps.seekTime)
+		) {
+			this.updateCanvas(nextProps);
+		}
+
+		function frameForTime(t) {
+			return ~~(t * 60);
+		}
+	}
+
+	public findMostRecentMessage(seekTime) {
+		const { message } = this.props;
+		const { lastMessageIndex, lastSeekTime } = this.state;
+		let mostRecentMessageIndex = null;
+		if (seekTime >= lastSeekTime) {
+			for (let i = lastMessageIndex; i < message.entries.length; ++i) {
+				const msg = message.entries[i];
+				if (msg && msg.relTime >= seekTime) {
+					mostRecentMessageIndex = i;
+					break;
+				}
+			}
+		}
+
+		if (!mostRecentMessageIndex) {
+			// TODO this can be faster with binary search, not currently a bottleneck though.
+
+			mostRecentMessageIndex = message.entries.findIndex((e) => e.relTime >= seekTime);
+		}
+
+		if (mostRecentMessageIndex) {
+			this.setState({
+				lastMessageIndex: mostRecentMessageIndex,
+				lastSeekTime: seekTime,
+			});
+			return message.entries[mostRecentMessageIndex];
+		}
+	}
+
+	public updateCanvas(props) {
+		const { message, live, seekTime } = props;
+		if (!this.canvas || message.entries.length === 0) {
+			return;
+		}
+
+		let mostRecentMsg = message.entries[message.entries.length - 1];
+		if (!live) {
+			mostRecentMsg = this.findMostRecentMessage(seekTime);
+
+			if (!mostRecentMsg) {
+				mostRecentMsg = message.entries[0];
+			}
+		}
+
+		const ctx = this.canvas.getContext(`2d`);
+		// ctx.clearRect(0, 0, 180, 15);
+
+		for (let i = 0; i < message.byteStateChangeCounts.length; ++i) {
+			const hexData = mostRecentMsg.hexData.substr(i * 2, 2);
+			ctx.fillStyle = message.byteColors[i];
+
+			ctx.fillRect(i * 20, 0, 20, 15);
+
+			ctx.font = `12px Courier`;
+			ctx.fillStyle = `white`;
+			if (hexData) {
+				ctx.fillText(hexData, i * 20 + 2, 12);
+			} else {
+				ctx.fillText(`-`, i * 20 + 7, 12);
+			}
+		}
+	}
+
+	public onVisibilityChange(isVisible) {
+		if (isVisible !== this.state.isVisible) {
+			this.setState({ isVisible });
+		}
+	}
+
+	public onCanvasRefAvailable(ref: HTMLCanvasElement) {
+		if (!ref) {
+			return;
+		}
+
+		this.canvas = ref;
+		this.canvas.width = window.devicePixelRatio * 160;
+		this.canvas.height = window.devicePixelRatio * 15;
+		const ctx = this.canvas.getContext(`2d`);
+		ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+	}
+
+	public render() {
+		return <canvas ref={this.onCanvasRefAvailable} className="cabana-meta-messages-list-item-bytes-canvas" />;
+	}
+}
