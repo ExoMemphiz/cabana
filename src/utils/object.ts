65420861f4440c2e49bab9e76660c8a53f41db58
Chris Rosendorf <Chris@rosendorf.dk>
2020-04-03 14:34:31 +0200

    Added @types in package.json & converted object to .ts

---
+++ src/utils/object.ts
@@ -0,0 +1,18 @@
+export function swapKeysAndValues(obj: object) {
+	return Object.keys(obj).reduce((acc, k) => {
+		// @ts-ignore
+		acc[obj[k]] = k;
+		return acc;
+	});
+}
+
+export function fromArray(arr: Array<[string, number]>) {
+	// arr is an array of array key-value pairs
+	// like [['a', 1], ['b', 2]]
+
+	const pairs = arr.map(([k, v]) => ({ [k]: v }));
+	if (pairs.length > 0) {
+		return Object.assign({}, ...pairs);
+	}
+	return {};
+}
